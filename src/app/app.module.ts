a24904c8bcdc2d8f9d9c9b3509f6fbb2f741a795
Abu1234 <37133919+Abu1234@users.noreply.github.com>
2021-11-26 12:10:17 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+
+
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { Routes, RouterModule } from "@angular/router";
+import { AppComponent } from "./app.component";
+
+import { HomeComponent } from "../home/home.component";
+import { MaterialComponent } from "../material/material.component";
+const ROUTES: Routes = [
+  { path: "home", component: HomeComponent },
+  { path: "material", component: MaterialComponent },
+
+  { path: "**", redirectTo: "home" }
+];
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+
+    RouterModule.forRoot(ROUTES)
+  ],
+  declarations: [AppComponent, HomeComponent, MaterialComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
